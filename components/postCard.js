92bcf62e27d41b765eb325ae978c189eb30a306c
Matthew Maloney <matthewgmaloney@gmail.com>
2023-12-14 18:42:10 -0600

    basic postCard written

---
+++ components/postCard.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function PostCard({postObj}) {
+    return (
+        <div className="post-card">
+            <img className="post-card-img" src="{post.image_url}" />
+            <Link passHref href={`posts/${post.id}`}><h3 className='post-card-title'>post.title</h3></Link>
+            <p className="post-card-category">post.category.label</p>
+            <p className="post-card-author">post.user.id</p>
+        </div>
+    )
+}
