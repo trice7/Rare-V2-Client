f3f3ea25f76667655f1b193538c58d6cbde9d677
Matthew Maloney <matthewgmaloney@gmail.com>
2023-12-16 10:49:54 -0600

    fixed register form

--- components/RegisterForm.js
+++ components/RegisterForm.js
@@ -19,12 +19,12 @@ function RegisterForm({ user, updateUser }) {
   return (
     <Form onSubmit={handleSubmit}>
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control as="textarea" name="username" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
-        <Form.Text className="text-muted">Username:</Form.Text>
+        <Form.Label>Gamer Username</Form.Label>
+        <Form.Control as="textarea" name="username" required placeholder="Enter your username" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        <Form.Text className="text-muted"></Form.Text>
         <Form.Label>Gamer Bio</Form.Label>
         <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
         <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
-        <Form.Label>Gamer Username</Form.Label>
       </Form.Group>
       <Button variant="primary" type="submit">
         Submit
--- components/forms/postForm.js
+++ components/forms/postForm.js
@@ -1,5 +1,35 @@
-// export default function PostForm() {
-//   return (
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
-//   )
-// }
+function PostForm() {
+  const [formData, setFormData] = useState({
+    categoryId: '',
+    uid: user.uid,
+    title: '',
+    imageUrl: '',
+    content: '',
+    approved: true
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    registerUser(formData).then(() => updateUser(user.uid));
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Control as="textarea" name="username" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        <Form.Text className="text-muted">Username:</Form.Text>
+        <Form.Label>Gamer Bio</Form.Label>
+        <Form.Control as="textarea" name="bio" required placeholder="Enter your Bio" onChange={({ target }) => setFormData((prev) => ({ ...prev, [target.name]: target.value }))} />
+        <Form.Text className="text-muted">Let other gamers know a little bit about you...</Form.Text>
+        <Form.Label>Gamer Username</Form.Label>
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
